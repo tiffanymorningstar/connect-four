491c46abcc459ea20ceca48d28c078383687a40d
trmorningstar <trmorningstar@gmail.com>
2022-09-12 12:18:31 -0400

    add music

--- js/app.js
+++ js/app.js
@@ -27,6 +27,9 @@ let winningArray = [
   [11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34]
 ];
 
+const mortalKombat = new Audio("../assets/audio/mkaudio.mp3")
+
+
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -41,8 +44,23 @@ const messageEls = document.querySelector("#message")
 
 const boardEl = document.querySelector('.board')
 
+const resetBtnEl = document.querySelector("#reset-button")
+
+
+const mklogoImg = document.querySelector("#mklogo")
+console.log(mklogoImg)
+
 /*----------------------------- Event Listeners -----------------------------*/
 boardEl.addEventListener('click', handleClick)
+
+
+resetBtnEl.addEventListener('click', init)
+
+mklogoImg.addEventListener('click', function(evt) {
+  console.log(evt.target)
+})
+
+
 
 
 /*-------------------------------- Functions
@@ -58,7 +76,6 @@ function init() {
     null, null, null, null, null, null, null,
     null, null, null, null, null, null, null
   ]
-  console.log(board)
   turn = 1
   winner = null
   render()
@@ -70,6 +87,8 @@ function render() {
       openingEls[idx].textContent = "Z"
     } else if (opening === -1) {
       openingEls[idx].textContent = "Q"
+    } else {
+      openingEls[idx].textContent = ''
     }
   })
 
@@ -147,20 +166,11 @@ let winnersCombo = bestCombo.some(function(value){
   return null
 }
 
-// 6b) Attach an event listener to the game board (you can do this to each // one of the existing `openingEls` OR add a new cached element reference // that will allow you to take advantage of event bubbling). On the // `'click'` event, it should call the `handleClick` function // you created in 6a. 
-// 6c) Obtain the index of the opening that was clicked by "extracting" the // index from an `id` assigned to the element in the HTML. Assign this // to a constant called `opIdx`. 
-// 6d) If the `board` has a value at the `opIdx`, immediately `return` // because that square is already taken. Also, if `winner` is not `null` // immediately `return` because the game is over. 
-// 6e) Update the `board` array at the `opIdx` with the current value of // `turn`. 
-// 6f) Change the turn by multiplying `turn` by `-1` (this flips a `1` to // `-1`, and vice-versa). 
-// 6g) Set the `winner` variable if there's a winner by calling a new // function: `getWinner`. 
-// 6h) All the state has been updated so we need to render our updated state // to the user by calling the `render` function we wrote earlier. 
+mklogoImg.addEventListener("click", function(evt){
+  mortalKombat.volume = .10
+  mortalKombat.play()
+})
 
-// Step 7 - Build the `getWinner` function 
-// 7a) Create a function called `getWinner` /* * There are two methods you can use to find out if there is a winner. * * Step b1 below is a more elegant method that takes advantage of the * `winningCombos` array you wrote above in step 5. * * Step b2 might be a little simpler to comprehend, but you'll need to write * more code. Step b2 also won't take advantage of the `winningCombos` * array, but using it as a reference will help you build a solution. * ***Ensure you choose only one path.*** *
-// 7b1)Loop through each of the winning combination arrays defined in the // `winningCombos` array. Total up the three board positions using the // three indexes in the current combo. Convert the total to an absolute // value (convert any negative total to positive). If the total equals 3, // we have a winner! Set the `winner` variable to the board's value at // the index specified by the first index of that winning combination's // array by returning that value. 
-// 7b2)For each one of the winning combinations you wrote in step 5, find the // total of each winning combination. Convert the total to an absolute // value (convert any negative total to positive). If the total equals 3, // we have a winner! Set the `winner` variable to the board's value at // the index specified by the first index of that winning combination's // array by returning that value. 
-// 7c) If there is no winner, check to see if there is a tie. Set the // `winner` variable to `'T'` if there are no more nulls in the board // array byreturning the string `'T'`. 
-// 7d) If there is no winner and there isn’t a tie, return `null`. 
 
 // Step 8 - Create Reset functionality 
 // 8a) Add a reset button to the HTML document. // 8b) Store the new reset button element in a constant named `resetBtnEl`. // 8c) Attach an event listener to the `resetBtnEl`. On the `'click'` event // it should call the `init` function you created in 3.
